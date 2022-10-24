806cfe945f2d4be7a19f557d62ab9cf16b29ba9b
Philipp Schlesinger <info@philcomm.dev>
2022-10-24 15:27:53 +0200

    Initial commit

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem "dotenv", "~> 2.8"
 gem "discordrb", "~> 3.4"
+gem "httparty", "~> 0.20"
--- bot.rb
+++ bot.rb
@@ -1,17 +1,67 @@
 require "pry"
+require "open3"
 require "dotenv"
+require "httparty"
+require "tempfile"
 require "discordrb"
 
 Dotenv.load
 
 bot = Discordrb::Bot.new(token: ENV["TOKEN"])
 
-bot.message in: "#testies" do |event|
-  event.respond "you mfer!"
-end
+ESRGAN_HOME = ENV["ESRGAN_HOME"]
 
-bot.mention do |event|
-  event.respond "Who dares to disturb me?"
+bot.mention in: "#screenshots" do |event|
+  unless event.message.attachments.any?
+    event.respond "I can only upscale an image if you attach one to the message you ping me in"
+    next
+  end
+
+  unless event.message.attachments.one?
+    event.respond "Please send only one image per message for now (this is being worked on)"
+    next
+  end
+
+  attachment = event.message.attachments.first
+
+  unless attachment.image?
+    event.respond "Please attach an image"
+    next
+  end
+
+  image_file_type = attachment.filename.match(/\.(?<type>png|jpg|jpeg)$/)[:type]
+
+  Tempfile.create(["2cho", ".#{image_file_type}"], "tmp/input") do |input_file|
+    input_file.write HTTParty.get(attachment.url).body
+
+    event.message.reply! "Starting upscale, this can take a while"
+
+    _stdout, stderr, status = Open3.capture3(
+      "python3 #{ESRGAN_HOME}/inference_realesrgan.py -n RealESRGAN_x4plus -i #{input_file.path} -o tmp/output --fp32 --ext png --suffix \"\""
+    )
+
+    if status.exitstatus != 0
+      event.message.reply! "Something went wrong"
+      puts stderr
+    else
+      output_file_name = "#{File.basename(input_file, ".*")}.png"
+
+      output_file = File.new(File.join("tmp", "output", output_file_name))
+
+      messages = [
+        "I got you covered!",
+        "Here you go!",
+        "Enjoy!"
+      ]
+
+      event.message.reply!(
+        messages.sample,
+        attachments: [output_file]
+      )
+
+      File.delete(output_file)
+    end
+  end
 end
 
 bot.run
