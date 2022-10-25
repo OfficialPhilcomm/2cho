6b03c4a66cafb2f4ebf3dbca9643238b2939e58d
Philipp Schlesinger <info@philcomm.dev>
2022-10-25 13:55:17 +0200

    Use cli command

--- bot.rb
+++ bot.rb
@@ -36,18 +36,17 @@ bot.mention in: "#screenshots" do |event|
 
     event.message.reply! "Starting upscale, this can take a while"
 
+    output_file_name = "#{File.basename(input_file, ".*")}.png"
+    output_file = File.new(File.join("tmp", "output", output_file_name))
+
     _stdout, stderr, status = Open3.capture3(
-      "python3 #{ESRGAN_HOME}/inference_realesrgan.py -n RealESRGAN_x4plus -i #{input_file.path} -o tmp/output --fp32 --ext png --suffix \"\""
+      "realesrgan-ncnn-vulkan -i #{input_file.path} -o #{output_file}"
     )
 
     if status.exitstatus != 0
       event.message.reply! "Something went wrong"
       puts stderr
     else
-      output_file_name = "#{File.basename(input_file, ".*")}.png"
-
-      output_file = File.new(File.join("tmp", "output", output_file_name))
-
       messages = [
         "I got you covered!",
         "Here you go!",
