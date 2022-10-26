98430bc052ca0f01a34ed3933e43cdf2daa07508
Philipp Schlesinger <info@philcomm.dev>
2022-10-26 13:03:28 +0200

    Use file storage if file is too big

--- lib/upscale_bot.rb
+++ lib/upscale_bot.rb
@@ -1,4 +1,5 @@
 require "pry"
+require "time"
 require "open3"
 require "dotenv"
 require "httparty"
@@ -42,37 +43,39 @@ module TwoCho
 
         image_file_type = attachment.filename.match(/\.(?<type>png|jpg|jpeg)$/)[:type]
 
+        event.message.reply! "Starting upscale, this can take a while"
+
         Tempfile.create(["2cho", ".#{image_file_type}"], "tmp/input") do |input_file|
           input_file.write HTTParty.get(attachment.url).body
 
-          event.message.reply! "Starting upscale, this can take a while"
-
           output_file_path = File.join("tmp", "output", "#{File.basename(input_file, ".*")}.png")
 
-          _stdout, stderr, status = Open3.capture3(
-            "#{ENV["ESRGAN_HOME"]}/realesrgan-ncnn-vulkan -i #{input_file.path} -o #{output_file_path} -s 2"
-          )
+          success = upscale_image input_flie.path, output_file_path
 
-          if status.exitstatus != 0
+          unless success
             event.message.reply! "Something went wrong"
-            puts stderr
-          else
-            output_file = File.new(output_file_path)
-
-            begin
-              event.message.reply!(
-                success_messages,
-                attachments: [output_file]
-              )
-            rescue RestClient::PayloadTooLarge
-              event.message.reply! "I am sorry, but the upscaled image is too big to upload"
-            end
-
-            File.delete(output_file)
+            next
           end
+
+          output_file = File.new(output_file_path)
+
+          if file_too_big? output_file
+            url = move_file_to_storage(output_file)
+
+            event.message.reply! url
+          else
+            event.message.reply!(
+              success_messages,
+              attachments: [output_file]
+            )
+          end
+
+          File.delete(output_file)
         end
       end
     end
+
+    private
 
     def success_messages
       [
@@ -80,6 +83,37 @@ module TwoCho
         "Here you go!",
         "Enjoy!"
       ].sample
+    end
+
+    def upscale_image(input_file_path, output_file_path)
+      _stdout, stderr, status = Open3.capture3(
+        "#{ENV["ESRGAN_HOME"]}/realesrgan-ncnn-vulkan -i #{input_file_path} -o #{output_file_path} -s 2"
+      )
+
+      if status.exitstatus != 0
+        puts stderr
+        false
+      else
+        true
+      end
+    end
+
+    def file_too_big?(file)
+      (file.size.to_f / 1024000) > 8
+    end
+
+    def move_file_to_storage(file)
+      folder = Date.today.strftime("%Y%m%d")
+
+      if !Dir.exist?(File.join(ENV["WEBSERVER_HOME"], folder))
+        Dir.mkdir File.join(ENV["WEBSERVER_HOME"], folder)
+      end
+
+      server_file_path = File.join(folder, File.basename(file))
+
+      File.rename(file, File.join(ENV["WEBSERVER_HOME"], server_file_path))
+
+      "https://#{ENV["URL_BASE"]}/#{server_file_path}"
     end
   end
 end
