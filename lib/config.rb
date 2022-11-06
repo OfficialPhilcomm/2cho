9a420922a3ec83835f189bfa8a4c955e72df6643
Philipp Schlesinger <info@philcomm.dev>
2022-11-06 22:57:35 +0100

    Improve config system

--- lib/config.rb
+++ lib/config.rb
@@ -5,5 +5,25 @@ module TwoCho
     include YAMLConfig
 
     file "config.yml"
+
+    schema do
+      group :discord do
+        integer :application_id
+        string :public_key
+        string :token
+        list :allowed_servers
+      end
+
+      group :esrgan do
+        string :home
+      end
+
+      group :webserver do
+        string :home
+        string :domain
+      end
+    end
   end
 end
+
+TwoCho::Config.load
