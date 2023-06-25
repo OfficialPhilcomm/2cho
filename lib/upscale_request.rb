811f527eac5f10d625fc90fbec2a0684f72b2d7b
Philipp Schlesinger <info@philcomm.dev>
2023-06-25 08:59:26 +0200

    Remove debug message

--- lib/upscale_request.rb
+++ lib/upscale_request.rb
@@ -114,8 +114,6 @@ module TwoCho
           "#{TwoCho::Config.esrgan.executable} -i #{input_file_path} -o #{output_file_path} -s 2"
         )
 
-        discord_event.message.reply! "Upscale took #{Time.now - start_time} seconds"
-
         if status.exitstatus != 0
           puts "Something went wrong. Error: #{stderr}"
           false
